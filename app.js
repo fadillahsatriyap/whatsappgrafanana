7763be6cd94fc0f960855f6b37ab1a4df2c01d70
satriya <fadillahsatriya@gmail.com>
2022-12-02 09:05:06 +0700

    redeploy tesaa

--- app.js
+++ app.js
@@ -226,7 +226,16 @@ app.post('/send-message', [
       message: errors.mapped()
     });
   }
-  const str = req.body.message;
+  const str = `*Firing*
+  Value: [ metric='foo' labels={instance=bar} value=10 ]
+  Labels:
+  - alertname = TestAlert
+  - instance = Grafana
+  Annotations:
+  - description = number : 089686601193 message : absasaxdas 
+  - summary = Notification test
+  Silence: http://dev-middleware-api.hanabank.co.id/mdw-monitoring/alerting/silence/new?alertmanager=grafana&matcher=alertname%3DTestAlert&matcher=instance%3DGrafana
+  '`;
   //const words = str.split(' ');
   console.log('====>',str,'<========');
   const number = phoneNumberFormatter(str.split(':')[4].split(' ')[1]);
@@ -308,13 +317,31 @@ const findGroupByName = async function(name) {
 
 app.post('/send-group-message', [
   body('id').custom((value, { req }) => {
-    console.log(req.body)
-  
-    // console.log('====>',req,'<======');
-    if (!value && !req.body.name) {
-      throw new Error('Invalid value, you can use `id` or `name`');
-    }
-    return true;
+
+  //   const msgGroup = `*Firing*
+  //   Value: [ metric='foo' labels={instance=bar} value=10 ]
+  //   Labels:
+  //   - alertname = TestAlert
+  //   - instance = Grafana
+  //   Annotations:
+  //   - description = name : oi message : absasaxdas 
+  //   - summary = Notification test
+  //   Silence: http://dev-middleware-api.hanabank.co.id/mdw-monitoring/alerting/silence/new?alertmanager=grafana&matcher=alertname%3DTestAlert&matcher=instance%3DGrafana
+  //   '`;
+  // //const words = str.split(' ');
+  // console.log('====>',msgGroup,'<========');
+  // const name =   msgGroup.split(':')[4].split(' ')[1];
+  // const message = msgGroup.split(':')[5].split('\n')[0];
+  // console.log(name,'<1')
+  // console.log(message,'<2')
+  //   // console.log('====>',req,'<======');
+    // if (!value && !name) {
+    //   throw new Error('Invalid value, you can use `id` or `name`');
+    // }
+
+   
+
+  return true;
   }),
   body('message').notEmpty(),
   
@@ -344,10 +371,31 @@ app.post('/send-group-message', [
       message: errors.mapped()
     });
   }
+   
+  
+  const msgGroup = req.body.message;
+  
+  
+  // `*Firing*
+  // Value: [ metric='foo' labels={instance=bar} value=10 ]
+  // Labels:
+  // - alertname = TestAlert
+  // - instance = Grafana
+  // Annotations:
+  // - description = name : oi message : absasaxdas 
+  // - summary = Notification test
+  // Silence: http://dev-middleware-api.hanabank.co.id/mdw-monitoring/alerting/silence/new?alertmanager=grafana&matcher=alertname%3DTestAlert&matcher=instance%3DGrafana
+  // '`;
+  //const words = str.split(' ');
+  const groupName =   msgGroup.split(':')[4].split(' ')[1];
+  const message = msgGroup.split(':')[5].split('\n')[0];
+  console.log(groupName,'<1')
+  console.log(message,'<2')
+  // console.log('====>',req,'<======');
 
   let chatId = req.body.id;
-  const groupName = req.body.name;
-  const message = req.body.message;
+  // const groupName = req.body.name;
+  // const message = req.body.message;
 
   // Find the group by name
   if (!chatId) {
