fe39f267f2f94bd1070a1a203519ac4031a8a3c7
satriya <fadillahsatriya@gmail.com>
2022-12-05 09:37:27 +0700

    redeploy tesasaa

--- app.js
+++ app.js
@@ -31,13 +31,7 @@ app.use(express.urlencoded({
   extended: true
 }));
 
-/**
- * BASED ON MANY QUESTIONS
- * Actually ready mentioned on the tutorials
- * 
- * Many people confused about the warning for file-upload
- * So, we just disabling the debug for simplicity.
- */
+
 app.use(fileUpload({
   debug: false
 }));
@@ -68,7 +62,7 @@ const client = new Client({
       '--disable-accelerated-2d-canvas',
       '--no-first-run',
       '--no-zygote',
-      '--single-process', // <- this one doesn't works in Windows
+      '--single-process',
       '--disable-gpu'
     ],
   },
@@ -182,20 +176,20 @@ const checkRegisteredNumber = async function(number) {
 
 
 
-app.get('/send',(req,res)=>
-{
-  let tujuan = req.query.tujuan;
-  let pesan  = req.query.pesan;
+// app.get('/send',(req,res)=>
+// {
+//   let tujuan = req.query.tujuan;
+//   let pesan  = req.query.pesan;
 
-  tujuan= tujuan.substring(1);
-  tujuan= '62${tujuan}@c.us';
-  console.log(tujuan);
-  client.sendMessage (tujuan, pesan);
-  res.json({status: false});
-  console.log(pesan);
+//   tujuan= tujuan.substring(1);
+//   tujuan= '62${tujuan}@c.us';
+//   console.log(tujuan);
+//   client.sendMessage (tujuan, pesan);
+//   res.json({status: false});
+//   console.log(pesan);
 
   
-});
+// });
 
 
 
@@ -273,9 +267,30 @@ app.post('/send-message', [
 
 // Send media
 app.post('/send-media', async (req, res) => {
-  const number = phoneNumberFormatter(req.body.number);
-  const caption = req.body.caption;
-  const fileUrl = req.body.file;
+  const str = `Firing
+  Value: [ metric='foo' labels={instance=bar} value=10 ]
+  Labels:
+  - alertname = TestAlert
+  - instance = Grafana
+  Annotations:
+  - description = number = 089686601193 caption = absasaxdas file = https://cdn.pixabay.com/photo/2020/06/21/18/23/pixabay-5326193_960_720.png
+  - summary = Notification test
+  Silence: http://dev-middleware-api.hanabank.co.id/mdw-monitoring/alerting/silence/new?alertmanager=grafana&matcher=alertname%3DTestAlert&matcher=instance%3DGrafana
+  '`;
+  //const words = str.split(' ');
+  console.log('====>',str,'<========');
+  const number = phoneNumberFormatter(str.split('=')[8].split(' ')[1]);
+  const caption = str.split('=')[9].split('file')[0];
+  const fileUrl = str.split('=')[10].split('\n')[0];
+  console.log(number,'<1')
+  console.log(fileUrl,'<2')
+
+
+
+  // const number1 = phoneNumberFormatter(req.body.number);
+  // console.log(req.body.number)
+  // const caption = req.body.caption;
+  // const fileUrl = req.body.file;
 
   // const media = MessageMedia.fromFilePath('./image-example.png');
   // const file = req.files.file;
@@ -314,50 +329,12 @@ const findGroupByName = async function(name) {
   return group;
 }
 
-
 app.post('/send-group-message', [
   body('id').custom((value, { req }) => {
-
-  //   const msgGroup = `*Firing*
-  //   Value: [ metric='foo' labels={instance=bar} value=10 ]
-  //   Labels:
-  //   - alertname = TestAlert
-  //   - instance = Grafana
-  //   Annotations:
-  //   - description = name : oi message : absasaxdas 
-  //   - summary = Notification test
-  //   Silence: http://dev-middleware-api.hanabank.co.id/mdw-monitoring/alerting/silence/new?alertmanager=grafana&matcher=alertname%3DTestAlert&matcher=instance%3DGrafana
-  //   '`;
-  // //const words = str.split(' ');
-  // console.log('====>',msgGroup,'<========');
-  // const name =   msgGroup.split(':')[4].split(' ')[1];
-  // const message = msgGroup.split(':')[5].split('\n')[0];
-  // console.log(name,'<1')
-  // console.log(message,'<2')
-  //   // console.log('====>',req,'<======');
-    // if (!value && !name) {
-    //   throw new Error('Invalid value, you can use `id` or `name`');
-    // }
-
-   
-
   return true;
   }),
   body('message').notEmpty(),
-  
 ]
-
-
-
-// body('number').notEmpty(),
-//   body('message').notEmpty(),
-// ], async (req, res) => {
-//   const errors = validationResult(req).formatWith(({
-//     msg
-//   }) => {
-//     return msg;
-//   });
-
 , async (req, res) => {
   const errors = validationResult(req).formatWith(({
     msg
@@ -372,25 +349,23 @@ app.post('/send-group-message', [
     });
   }
    
-  
-  const msgGroup = req.body.message;
-  
-  
-  // `*Firing*
-  // Value: [ metric='foo' labels={instance=bar} value=10 ]
-  // Labels:
-  // - alertname = TestAlert
-  // - instance = Grafana
-  // Annotations:
-  // - description = name : oi message : absasaxdas 
-  // - summary = Notification test
-  // Silence: http://dev-middleware-api.hanabank.co.id/mdw-monitoring/alerting/silence/new?alertmanager=grafana&matcher=alertname%3DTestAlert&matcher=instance%3DGrafana
-  // '`;
+  const msgGroup = `Firing
+  Value: [ metric='foo' labels={instance=bar} value=10 ]
+  Labels:
+  - alertname = TestAlert
+  - instance = Grafana
+  Annotations:
+  - description = name = oi caption = absasaxdas file = https://cdn.pixabay.com/photo/2020/06/21/18/23/pixabay-5326193_960_720.png
+  - summary = Notification test
+  Silence: http://dev-middleware-api.hanabank.co.id/mdw-monitoring/alerting/silence/new?alertmanager=grafana&matcher=alertname%3DTestAlert&matcher=instance%3DGrafana
+  '`;
   //const words = str.split(' ');
-  const groupName =   msgGroup.split(':')[4].split(' ')[1];
-  const message = msgGroup.split(':')[5].split('\n')[0];
+  const groupName =   msgGroup.split('=')[8].split(' ')[1];
+  const caption = msgGroup.split('=')[9].split('file')[0];
+  const fileUrl = msgGroup.split('=')[10].split('\n')[0];
   console.log(groupName,'<1')
-  console.log(message,'<2')
+  console.log(caption,'<2')
+  console.log(fileUrl,'<3')
   // console.log('====>',req,'<======');
 
   let chatId = req.body.id;
@@ -409,11 +384,25 @@ app.post('/send-group-message', [
     chatId = group.id._serialized;
   }
 
-  client.sendMessage(chatId, message).then(response => {
+  let mimetype;
+  const attachment = await axios.get(fileUrl, {
+    responseType: 'arraybuffer'
+  }).then(response => {
+    mimetype = response.headers['content-type'];
+    return response.data.toString('base64');
+  });
+
+  const media = new MessageMedia(mimetype, attachment, 'Media');
+  
+
+  client.sendMessage(chatId, media,  {
+    caption: caption
+  } ).then(response => {
     res.status(200).json({
       status: true,
       response: response
     });
+    console.log(caption,'<4')
   }).catch(err => {
     res.status(500).json({
       status: false,
@@ -421,6 +410,7 @@ app.post('/send-group-message', [
     });
   });
 });
+
 
 // Clearing message on spesific chat
 app.post('/clear-message', [
